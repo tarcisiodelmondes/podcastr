52a5f50f0c896bce731ae6b1978cac7b6b1a8507
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2021-04-22 16:49:48 -0300

    Aula 4

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,30 @@
+import Image from 'next/image';
+import { useContext, useEffect, useRef } from 'react';
+import { PlayerContext } from '../../context/PlayerContext';
 import styles from './styles.module.scss';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
 
 export function Player() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const {
+    currentEpisodeIndex,
+    episodeList,
+    isPlaying,
+    togglePlay,
+    setPlayingState,
+  } = useContext(PlayerContext);
+
+  const episode = episodeList[currentEpisodeIndex];
+
+  useEffect(() => {
+    if (!audioRef.current) return;
+
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
+
   return (
     <div className={styles.playerContainer}>
       <header>
@@ -8,37 +32,78 @@ export function Player() {
         <strong>Tocando agora</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
 
-      <footer className={styles.empty}>
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
+
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ backgroundColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
 
+        {episode && (
+          <audio
+            ref={audioRef}
+            src={episode.url}
+            autoPlay
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
+          />
+        )}
+
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar Anterior" />
           </button>
 
-          <button type="button" className={styles.playButton}>
-            <img src="/play.svg" alt="Tocar" />
+          <button
+            type="button"
+            disabled={!episode}
+            className={styles.playButton}
+            onClick={() => togglePlay()}
+          >
+            {isPlaying ? (
+              <img src="/pause.svg" alt="Pausar" />
+            ) : (
+              <img src="/play.svg" alt="Tocar" />
+            )}
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class Mydocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
         <Head>
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
